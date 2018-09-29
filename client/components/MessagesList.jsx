0fda161df20e738a7f2e67f2ae5698163d1668a3
alexeypant <alexeypant@gmail.com>
2018-09-29 19:43:05 +0300

    scroll to the bottom applied

--- client/components/MessagesList.jsx
+++ client/components/MessagesList.jsx
@@ -12,6 +12,18 @@ const mapStateToProps = (state) => {
 
 @connect(mapStateToProps, actionCreators)
 export default class MessagesList extends React.Component {
+  componentDidMount() {
+    this.scrollToTheBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToTheBottom();
+  }
+
+  scrollToTheBottom() {
+    this.elm.scrollTop = this.elm.scrollHeight;
+  }
+
   renderMessagesList = messages => messages.map(msg => (
     <div key={msg.id}>
       <b>{msg.userName}</b>
@@ -22,8 +34,11 @@ export default class MessagesList extends React.Component {
   render() {
     const { messages } = this.props;
     return (
-      <div style={{ height: '70vh', width: '80%', overflow: 'auto' }}>
-        <div className="d-flex flex-column mb-3 h-100 justify-content-end">
+      <div
+        style={{ height: '70vh', width: '80%', overflow: 'scroll' }}
+        ref={(elm) => { this.elm = elm; }} // eslint-disable-line
+      >
+        <div className="d-flex flex-column mb-3 h-100 justify-content-start">
           {this.renderMessagesList(messages)}
         </div>
       </div>
